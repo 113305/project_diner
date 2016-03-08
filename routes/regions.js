3654e4c20582a19fa71be71f2e50b9d1c51d0b26
SUNMI <kitty0704@naver.com>
2016-03-08 16:32:15 +0900

    더미데이터 변수 수정

--- routes/regions.js
+++ routes/regions.js
@@ -6,10 +6,24 @@ router.get('/', function(req, res, next) {
     var result = {
         "results": {
             "message": "지역조회가 정상적으로 처리되었습니다.",
-            "data": [{
-                "regionName": "강남구",
-                "regionPhotoUrl": "https://s3.ap-northeast-2.amazonaws.com/diner-img/region/gangnam.png"
-            }]
+            "data":  [
+                {
+                    "region_name": "강남구",
+                    "region_photo_url": "https://s3.ap-northeast-2.amazonaws.com/diner-img/region/gangnam.png"
+                },
+                {
+                    "region_name": "종로구",
+                    "region_photo_url": "https://s3.ap-northeast-2.amazonaws.com/diner-img/region/jongno.png"
+                },
+                {
+                    "region_name": "마포구",
+                    "region_photo_url": "https://s3.ap-northeast-2.amazonaws.com/diner-img/region/mapo.png"
+                },
+                {
+                    "region_name": "송파구",
+                    "region_photo_url": "https://s3.ap-northeast-2.amazonaws.com/diner-img/region/songpa.png"
+                }
+            ]
         }
     };
     res.json(result);

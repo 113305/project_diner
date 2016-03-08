0eb8c48076c761e2e79e24dc8d302d74e730ba72
SUNMI <kitty0704@naver.com>
2016-03-08 15:59:18 +0900

    더미데이터 변수 수정

--- routes/regions.js
+++ routes/regions.js
@@ -33,7 +33,6 @@ router.get('/:regionId', function(req, res, next) {
                         "restaurantId": 2,
                         "restaurantName": "마티아",
                         "address": "서울특별시 강남구 삼성동 144번지",
-                        "restaurantPhone": "070-8029-3020",
                         "businessHours": "매일14:00~22:00 (수요일 휴무)",
                         "websiteUrl": "http://mattia.com",
                         "rewardPhotoUrl": "https://s3.ap-northeast-2.amazonaws.com/diner-img/restaurant/mattia/reward_photo/mattia_reward.jpg",
@@ -47,122 +46,122 @@ router.get('/:regionId', function(req, res, next) {
                         "restaurantInfo": "특등급 생고기를 직접 두들겨만든 수제 돈까스 전문점",
                         "restaurantPhotoUrl": [
                             {
-                                "restaurant_photo_url": "https://s3.ap-northeast-2.amazonaws.com/diner-img/restaurant/mattia/restaurant_photo/mattia1.jpg"
+                                "restaurantPhotoUrl": "https://s3.ap-northeast-2.amazonaws.com/diner-img/restaurant/mattia/restaurant_photo/mattia1.jpg"
                             },
                             {
-                                "restaurant_photo_url": "https://s3.ap-northeast-2.amazonaws.com/diner-img/restaurant/mattia/restaurant_photo/mattia2.jpg"
+                                "restaurantPhotoUrl": "https://s3.ap-northeast-2.amazonaws.com/diner-img/restaurant/mattia/restaurant_photo/mattia2.jpg"
                             },
                             {
-                                "restaurant_photo_url": "https://s3.ap-northeast-2.amazonaws.com/diner-img/restaurant/mattia/restaurant_photo/mattia3.png"
+                                "restaurantPhotoUrl": "https://s3.ap-northeast-2.amazonaws.com/diner-img/restaurant/mattia/restaurant_photo/mattia3.png"
                             },
                             {
-                                "restaurant_photo_url": "https://s3.ap-northeast-2.amazonaws.com/diner-img/restaurant/mattia/restaurant_photo/mattia_main.jpg"
+                                "restaurantPhotoUrl": "https://s3.ap-northeast-2.amazonaws.com/diner-img/restaurant/mattia/restaurant_photo/mattia_main.jpg"
                             }
                         ],
                         "menu": [
                             {
-                                "menu_class_id": 1,
-                                "menu_class_name": "에피타이저",
-                                "menu_name": "연어 샐러드",
-                                "menu_photo_url": "https://s3.ap-northeast-2.amazonaws.com/diner-img/restaurant/mattia/menu_photo/salmon_salad.JPG",
+                                "menuClassId": 1,
+                                "menuClassName": "에피타이저",
+                                "menuName": "연어 샐러드",
+                                "menuPhotoUrl": "https://s3.ap-northeast-2.amazonaws.com/diner-img/restaurant/mattia/menu_photo/salmon_salad.JPG",
                                 "price": 8000,
-                                "main_ingredient": "연어, 양배추, 사과 드레싱"
+                                "mainIngredient": "연어, 양배추, 사과 드레싱"
                             },
                             {
-                                "menu_class_id": 1,
-                                "menu_class_name": "에피타이저",
-                                "menu_name": "양송이 스프",
-                                "menu_photo_url": null,
+                                "menuClassId": 1,
+                                "menuClassName": "에피타이저",
+                                "menuName": "양송이 스프",
+                                "menuPhotoUrl": null,
                                 "price": 4000,
-                                "main_ingredient": null
+                                "mainIngredient": null
                             },
                             {
-                                "menu_class_id": 2,
-                                "menu_class_name": "메인디쉬",
-                                "menu_name": "등심돈까스",
-                                "menu_photo_url": "https://s3.ap-northeast-2.amazonaws.com/diner-img/restaurant/mattia/menu_photo/tenderloin_cutlet.jpg",
+                                "menuClassId": 2,
+                                "menuClassName": "메인디쉬",
+                                "menuName": "등심돈까스",
+                                "menuPhotoUrl": "https://s3.ap-northeast-2.amazonaws.com/diner-img/restaurant/mattia/menu_photo/tenderloin_cutlet.jpg",
                                 "price": 12000,
-                                "main_ingredient": "돼지 등심, 돈까스 소스"
+                                "mainIngredient": "돼지 등심, 돈까스 소스"
                             },
                             {
-                                "menu_class_id": 2,
-                                "menu_class_name": "메인디쉬",
-                                "menu_name": "안심돈까스",
-                                "menu_photo_url": "https://s3.ap-northeast-2.amazonaws.com/diner-img/restaurant/mattia/menu_photo/sirloin_cutlet.jpg",
+                                "menuClassId": 2,
+                                "menuClassName": "메인디쉬",
+                                "menuName": "안심돈까스",
+                                "menuPhotoUrl": "https://s3.ap-northeast-2.amazonaws.com/diner-img/restaurant/mattia/menu_photo/sirloin_cutlet.jpg",
                                 "price": 12000,
-                                "main_ingredient": "돼지 안심, 돈까스 소스"
+                                "mainIngredient": "돼지 안심, 돈까스 소스"
                             },
                             {
-                                "menu_class_id": 2,
-                                "menu_class_name": "메인디쉬",
-                                "menu_name": "훈제 연어까스",
-                                "menu_photo_url": "https://s3.ap-northeast-2.amazonaws.com/diner-img/restaurant/mattia/menu_photo/salmon_cutlet.jpg",
+                                "menuClassId": 2,
+                                "menuClassName": "메인디쉬",
+                                "menuName": "훈제 연어까스",
+                                "menuPhotoUrl": "https://s3.ap-northeast-2.amazonaws.com/diner-img/restaurant/mattia/menu_photo/salmon_cutlet.jpg",
                                 "price": 11000,
-                                "main_ingredient": "훈제연어, 타르타르소스 "
+                                "mainIngredient": "훈제연어, 타르타르소스 "
                             },
                             {
-                                "menu_class_id": 2,
-                                "menu_class_name": "메인디쉬",
-                                "menu_name": "함박 스테이크",
-                                "menu_photo_url": "https://s3.ap-northeast-2.amazonaws.com/diner-img/restaurant/mattia/menu_photo/hamburger_steak.jpg",
+                                "menuClassId": 2,
+                                "menuClassName": "메인디쉬",
+                                "menuName": "함박 스테이크",
+                                "menuPhotoUrl": "https://s3.ap-northeast-2.amazonaws.com/diner-img/restaurant/mattia/menu_photo/hamburger_steak.jpg",
                                 "price": 14000,
-                                "main_ingredient": "소고기, 체다치즈, 양파"
+                                "mainIngredient": "소고기, 체다치즈, 양파"
                             },
                             {
-                                "menu_class_id": 2,
-                                "menu_class_name": "메인디쉬",
-                                "menu_name": "모짜렐라 치즈 돈까스",
-                                "menu_photo_url": "https://s3.ap-northeast-2.amazonaws.com/diner-img/restaurant/mattia/menu_photo/cheese_cutlet.jpg",
+                                "menuClassId": 2,
+                                "menuClassName": "메인디쉬",
+                                "menuName": "모짜렐라 치즈 돈까스",
+                                "menuPhotoUrl": "https://s3.ap-northeast-2.amazonaws.com/diner-img/restaurant/mattia/menu_photo/cheese_cutlet.jpg",
                                 "price": 13000,
-                                "main_ingredient": "모짜렐라 치즈, 참깨 드레싱"
+                                "mainIngredient": "모짜렐라 치즈, 참깨 드레싱"
                             },
                             {
-                                "menu_class_id": 2,
-                                "menu_class_name": "메인디쉬",
-                                "menu_name": "우동",
-                                "menu_photo_url": null,
+                                "menuClassId": 2,
+                                "menuClassName": "메인디쉬",
+                                "menuName": "우동",
+                                "menuPhotoUrl": null,
                                 "price": 7500,
-                                "main_ingredient": null
+                                "mainIngredient": null
                             },
                             {
-                                "menu_class_id": 3,
-                                "menu_class_name": "디저트",
-                                "menu_name": "감자튀김",
-                                "menu_photo_url": null,
+                                "menuClassId": 3,
+                                "menuClassName": "디저트",
+                                "menuName": "감자튀김",
+                                "menuPhotoUrl": null,
                                 "price": 4500,
-                                "main_ingredient": "감자"
+                                "mainIngredient": "감자"
                             },
                             {
-                                "menu_class_id": 4,
-                                "menu_class_name": "음료",
-                                "menu_name": "콜라",
-                                "menu_photo_url": null,
+                                "menuClassId": 4,
+                                "menuClassName": "음료",
+                                "menuName": "콜라",
+                                "menuPhotoUrl": null,
                                 "price": 1000,
-                                "main_ingredient": null
+                                "mainIngredient": null
                             },
                             {
-                                "menu_class_id": 4,
-                                "menu_class_name": "음료",
-                                "menu_name": "사이다",
-                                "menu_photo_url": null,
+                                "menuClassId": 4,
+                                "menuClassName": "음료",
+                                "menuName": "사이다",
+                                "menuPhotoUrl": null,
                                 "price": 1000,
-                                "main_ingredient": null
+                                "mainIngredient": null
                             },
                             {
-                                "menu_class_id": 4,
-                                "menu_class_name": "음료",
-                                "menu_name": "맥주",
-                                "menu_photo_url": null,
+                                "menuClassId": 4,
+                                "menuClassName": "음료",
+                                "menuName": "맥주",
+                                "menuPhotoUrl": null,
                                 "price": 4000,
-                                "main_ingredient": null
+                                "mainIngredient": null
                             },
                             {
-                                "menu_class_id": 4,
-                                "menu_class_name": "음료",
-                                "menu_name": "소주",
-                                "menu_photo_url": null,
+                                "menuClassId": 4,
+                                "menuClassName": "음료",
+                                "menuName": "소주",
+                                "menuPhotoUrl": null,
                                 "price": 3500,
-                                "main_ingredient": null
+                                "mainIngredient": null
                             }
                         ]
                     }

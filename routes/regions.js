be10d0606bd59eb626f3e5203562ce9ecae2421e
SUNMI <kitty0704@naver.com>
2016-03-09 13:34:46 +0900

    더미데이터 변수 수정

--- routes/regions.js
+++ routes/regions.js
@@ -8,20 +8,20 @@ router.get('/', function(req, res, next) {
             "message": "지역조회가 정상적으로 처리되었습니다.",
             "data":  [
                 {
-                    "region_name": "강남구",
-                    "region_photo_url": "https://s3.ap-northeast-2.amazonaws.com/diner-img/region/gangnam.png"
+                    "regionName": "강남구",
+                    "regionPhotoUrl": "https://s3.ap-northeast-2.amazonaws.com/diner-img/region/gangnam.png"
                 },
                 {
-                    "region_name": "종로구",
-                    "region_photo_url": "https://s3.ap-northeast-2.amazonaws.com/diner-img/region/jongno.png"
+                    "regionName": "종로구",
+                    "regionPhotoUrl": "https://s3.ap-northeast-2.amazonaws.com/diner-img/region/jongno.png"
                 },
                 {
-                    "region_name": "마포구",
-                    "region_photo_url": "https://s3.ap-northeast-2.amazonaws.com/diner-img/region/mapo.png"
+                    "regionName": "마포구",
+                    "regionPhotoUrl": "https://s3.ap-northeast-2.amazonaws.com/diner-img/region/mapo.png"
                 },
                 {
-                    "region_name": "송파구",
-                    "region_photo_url": "https://s3.ap-northeast-2.amazonaws.com/diner-img/region/songpa.png"
+                    "regionName": "송파구",
+                    "regionPhotoUrl": "https://s3.ap-northeast-2.amazonaws.com/diner-img/region/songpa.png"
                 }
             ]
         }
